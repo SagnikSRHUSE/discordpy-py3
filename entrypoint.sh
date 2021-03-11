efd14eac271ea07274977acf3f89b21fe39d19c4
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2021-03-11 22:26:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
 # Run the Server
-pip3 install -q -r requirements.txt
+pip3 install -q --no-cache-dir -r requirements.txt
 ${MODIFIED_STARTUP}
